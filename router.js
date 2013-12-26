9dd866bcd934bc4fff79dcfdcf5dffb3fcedeff4
Brendon Gonzalez <brendon@brandingbrand.com>
2013-12-26 04:05:12 -0500

    Delete test route.

--- router.js
+++ router.js
@@ -1,9 +1,7 @@
 exports.registerRoutes = function(app) {
   //Register controllers
-  var index = require('./controllers')(app)
-    , user = require('./controllers/user')(app);
+  var index = require('./controllers')(app);
   
   //Register routes
   app.get('/', index.index);
-  app.get('/users', user.list);
 }
